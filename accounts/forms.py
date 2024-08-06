45d42f0063dee9b07ff7ee700587b0c4f0ec3491
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-08-06 19:06:30 +0330

    create Custom User Change and Custom User Creation forms

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from .models import CustomUserModel
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUserModel
+        fields = ("username", "email", "age", "gender")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUserModel
+        fields = ("username", "email", "age", "gender")
