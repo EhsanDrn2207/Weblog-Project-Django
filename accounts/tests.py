0705bafd8ab45ae34126e5f948f846da53e99a1d
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-08-12 13:34:43 +0330

    add Sign Up test

--- accounts/tests.py
+++ accounts/tests.py
@@ -21,3 +21,25 @@ class HomePageTest(TestCase):
         self.assertContains(response, "Home Page")
 
 
+class SignUpPageTest(TestCase):
+    def setUp(self):
+        self.username = "username"
+        self.email = "newemail"
+
+    def test_signup_page_url(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_template_used(self):
+        response = self.client.get(reverse("signup"))
+        self.assertTemplateUsed(response, "registration/signup_page.html")
+
+    def test_signup_page_form(self):
+        user = get_user_model().objects.create_user(username=self.username, email=self.email)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "username")
+        self.assertEqual(get_user_model().objects.all()[0].email, "newemail")
