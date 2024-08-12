1084bb880304bdc0268f5d0fe79178127b0dcd4f
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-08-12 13:21:02 +0330

    add home page test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_url(self):
+        response = self.client.get("/home/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home_page.html")
+
+    def test_home_page_components(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Home Page")
+
+
