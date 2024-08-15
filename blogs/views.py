2a4dd5212195f0c0797535f78e0bbef9dd3505b5
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-08-15 17:53:57 +0330

    add Blog Detail View in blogs

--- blogs/views.py
+++ blogs/views.py
@@ -5,8 +5,14 @@ from .models import Blog
 
 class BlogListView(generic.ListView):
     model = Blog
-    template_name = 'blogs/book_list_page.html'
+    template_name = 'blogs/blog_list_page.html'
     context_object_name = 'blogs'
 
     def get_queryset(self):
         return Blog.objects.filter(status='pub')
+
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+    template_name = "blogs/blog_detail_page.html"
+    context_object_name = "blog"
