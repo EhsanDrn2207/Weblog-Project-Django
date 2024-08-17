634d15d83631202b3f1eee3b313972f9c70bf3d9
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-08-17 08:52:56 +0330

    add paginator in blog list

--- blogs/views.py
+++ blogs/views.py
@@ -8,6 +8,7 @@ class BlogListView(generic.ListView):
     model = Blog
     template_name = 'blogs/blog_list_page.html'
     context_object_name = 'blogs'
+    paginate_by = 4
 
     def get_queryset(self):
         return Blog.objects.filter(status='pub')
