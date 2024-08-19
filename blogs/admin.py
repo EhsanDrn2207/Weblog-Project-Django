3089fd7a987c47fd893a916b5e80da9cf3f3ec7d
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-08-19 10:35:50 +0330

    add Comment model in blog_detail_view

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Comment
 
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'create_datetime', 'status')
 
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'recommend', 'is_active')
+
+
 admin.site.register(Blog, BlogAdmin)
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Blog
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
+from .models import Blog
+from .forms import CommentForm
 
 
 class BlogListView(generic.ListView):
@@ -15,10 +19,31 @@ class BlogListView(generic.ListView):
         return Blog.objects.filter(status='pub')
 
 
-class BlogDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Blog
-    template_name = "blogs/blog_detail_page.html"
-    context_object_name = "blog"
+@login_required
+def blog_detail_view(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    blog_comments = blog.comment.all()
+    if request.method == 'POST':
+        comment = CommentForm(request.POST)
+        if comment.is_valid():
+            new_comment = comment.save(commit=False)
+            new_comment.user = request.user
+            new_comment.blog = blog
+            new_comment.save()
+            comment = CommentForm()
+    else:
+        comment = CommentForm()
+    return render(request, "blogs/blog_detail_page.html", context={
+        "blog": blog,
+        "blog_comments": blog_comments,
+        "comment_form": comment,
+    })
+
+
+# class BlogDetailView(LoginRequiredMixin, generic.DetailView):
+#     model = Blog
+#     template_name = "blogs/blog_detail_page.html"
+#     context_object_name = "blog"
 
 
 class BlogCreateView(LoginRequiredMixin, generic.CreateView):
