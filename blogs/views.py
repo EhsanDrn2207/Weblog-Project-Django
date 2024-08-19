5bcb0d39d5d219d826eeaaca796e65e42c6d5ed5
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-08-19 09:31:19 +0330

    add user authorization feature

--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Blog
 from django.urls import reverse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class BlogListView(generic.ListView):
@@ -28,15 +28,23 @@ class BlogCreateView(LoginRequiredMixin, generic.CreateView):
     context_object_name = "form"
 
 
-class BlogUpdateView(LoginRequiredMixin, generic.UpdateView):
+class BlogUpdateView(UserPassesTestMixin, LoginRequiredMixin, generic.UpdateView):
     model = Blog
     fields = ["title", "text", "author", "status"]
     template_name = "blogs/blog_update_page.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class BlogDeleteView(LoginRequiredMixin, generic.DeleteView):
+
+class BlogDeleteView(UserPassesTestMixin, LoginRequiredMixin, generic.DeleteView):
     model = Blog
     template_name = "blogs/blog_delete_page.html"
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
     def get_success_url(self):
         return reverse("blog_list")
