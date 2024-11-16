7ec2959916431cd0ce81e5d3e7360d7ca01390bc
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-11-16 23:01:05 +0330

    add drf-yasg in project

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from . import views
-from api.api_views import (
+from blogs.api.api_views import (
     BlogListAPIView,
     BlogDetailAPIView,
     CommentListAPIView,
     CommentDetailAPIView,
+)
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -19,4 +37,8 @@ urlpatterns = [
     path('api/blogs/<int:pk>/', BlogDetailAPIView.as_view(), name='api_blog_detail'),
     path('api/comments/', CommentListAPIView.as_view(), name='api_comment_list'),
     path('api/comments/<int:pk>/', CommentDetailAPIView.as_view(), name='api_comment_detail'),
+    
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
